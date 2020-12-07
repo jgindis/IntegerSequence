0b5a3800bf4356e9e6f4812d49a6b65bf1741a46
jgindis <jgindis20@stuy.edu>
2020-12-07 13:24:24 -0500

    Added methods

--- Range.java
+++ Range.java
@@ -1,4 +1,14 @@
 import java.util.NoSuchElementException;
 public class Range implements IntegerSequence {
 
+  private int start,end,current;
+
+  public Range(int start,  int end){  }
+  public void reset(){  }
+  public int length(){  }
+  public boolean hasNext(){  }
+
+  //@throws NoSuchElementException
+  public int next(){    }
+
 }
